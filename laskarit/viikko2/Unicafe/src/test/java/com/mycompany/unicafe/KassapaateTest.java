91fd5aab7c3b92ebffaf8c76af4cbdb2000995f5
Miika Kalliokoski <kall.miika@outlook.com>
2021-03-27 20:44:07 +0200

    Täydennettiin testejä ja eriytettiin kortilla maksu käteismaksun testaamisesta

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -18,11 +18,17 @@ public class KassapaateTest {
     @Test
     public void luotuKassaOlemassa(){
         assertNotNull(kassa);
+        assertEquals(100000, kassa.kassassaRahaa()); //100snt = €
+        assertEquals(0, kassa.edullisiaLounaitaMyyty());
+        assertEquals(0, kassa.maukkaitaLounaitaMyyty());
     }
 
     @Test
     public void kassassaRahaa(){
-        assertEquals(100000, kassa.kassassaRahaa());
+        kassa.syoMaukkaasti(400);
+        assertEquals(100400, kassa.kassassaRahaa());
+        kassa.syoEdullisesti(240);
+        assertEquals(100640, kassa.kassassaRahaa());
     }
 
     @Test
@@ -41,45 +47,56 @@ public class KassapaateTest {
     }
 
     @Test
-    public void syoMaukkaasti(){
-        //int
+    public void syoMaukkaastiKateisella(){
         assertEquals(0, kassa.syoMaukkaasti(400));
         assertEquals(100, kassa.syoMaukkaasti(500));
         assertEquals(100, kassa.syoMaukkaasti(100));
         assertEquals(2, kassa.maukkaitaLounaitaMyyty());
-        //Maksukortti
+        assertEquals(100800, kassa.kassassaRahaa());
+
+    }
+    @Test
+    public void syoMaukkaastiKortilla(){
         assertTrue(kassa.syoMaukkaasti(kortti));
         assertTrue(kassa.syoMaukkaasti(kortti));
         assertTrue(kassa.syoMaukkaasti(kortti));
         assertFalse(kassa.syoMaukkaasti(kortti));
-        assertEquals(5, kassa.maukkaitaLounaitaMyyty());
+        assertEquals(3, kassa.maukkaitaLounaitaMyyty());
+        assertEquals(101200, kassa.kassassaRahaa());
     }
 
     private void assertFalse(boolean syoMaukkaasti) {
     }
 
     @Test
-    public void syoEdullisesti(){
-        //int
+    public void syoEdullisestiKateisella(){
         assertEquals(0, kassa.syoEdullisesti(240));
         assertEquals(100, kassa.syoEdullisesti(340));
         assertEquals(100, kassa.syoEdullisesti(100));
         assertEquals(2, kassa.edullisiaLounaitaMyyty());
-        //Maksukortti
+        assertEquals(100480, kassa.kassassaRahaa());
+    }
+
+    @Test
+    public void syoEdullisestiKortilla(){
         assertTrue(kassa.syoEdullisesti(kortti));
         assertTrue(kassa.syoEdullisesti(kortti));
         assertTrue(kassa.syoEdullisesti(kortti));
         assertTrue(kassa.syoEdullisesti(kortti));
         assertTrue(kassa.syoEdullisesti(kortti));
         assertFalse(kassa.syoEdullisesti(kortti));
-        assertEquals(7, kassa.edullisiaLounaitaMyyty());
+        assertEquals(5, kassa.edullisiaLounaitaMyyty());
+        assertEquals(101200, kassa.kassassaRahaa());
     }
 
     @Test
     public void lataaRahaaKortille(){
         kassa.lataaRahaaKortille(kortti, 200);
         assertEquals(1400, kortti.saldo());
+        assertEquals(100200, kassa.kassassaRahaa());
+
         kassa.lataaRahaaKortille(kortti,-200);
         assertEquals(1400, kortti.saldo());
+        assertEquals(100200, kassa.kassassaRahaa());
     }
 }
